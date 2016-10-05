3c236828dabf43ea291cf576884f07765c407ec1
Tyler Timmins <timmins_tyler@yahoo.com>
2016-10-05 16:20:57 -0500

    Add files via upload

---
+++ Pracitce/Pracitce/Main.cpp
@@ -0,0 +1,23 @@
+//PROGRAM: Practice
+//AUTHOR: Tyler Timmins
+//CLASS: ITSE 1307 2002
+//DESCRIPTION: This program is practice creating,
+//	saving, opening, and moving c++ projects
+
+//preprocessor directives
+#include <iostream>
+using namespace std;
+
+//main function
+int main()
+{
+	//all of our code goes here inside of {}
+	cout << "Today is hump day!" << endl << endl;
+	cout << "Mike, Mike MIKE MIKE MIKE!" << endl << endl;
+	cout << "GUESS what day it is!..." << endl << endl;
+	cout << "IT'S HUMP DAY WHOOOP WHOOOOP" << endl << endl;
+	cout << "Let it go Phil, let it go..." << endl << endl;
+	cout << "LEAGUE FOR DAYZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ" << endl << endl;
+
+	return 0;
+}
